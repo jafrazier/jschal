cb1566b54b55fb57b1f95c3aab1fe153f3857f8f
jfrazier <johnfrazier1222@gmail.com>
2017-04-11 21:42:37 -0400

    js Challange

---
+++ scripts.js
@@ -0,0 +1,60 @@
+// Challenge 1
+// var x = 10;
+// var y = 20;
+
+// console.log (x + y);
+
+
+// Challenge 2
+
+// var dc = ["batman", "superman", "flash"];
+// var marvel = ["x-men", "avengers", "deadpool"];
+
+// var comics = [ dc, marvel ];
+
+// console.log (comics[0][1]);
+// console.log (comics[1][2]);
+
+// Challenge 3
+
+// var x = 100;
+
+// if (x < 100) {
+// 	console.log ("Your number is less than 100")
+// } 
+
+// else if (x > 100) {
+// 	console.log ("Your number is greater than 100")
+// }
+
+// else {
+// 	console.log ("Your number is equal to 100")
+// };
+
+// Challenge 4
+
+// function myNameIs(John){   
+//  	return alert(John); 
+//  };
+// myNameIs("john");
+
+
+
+var door = 2;
+
+ function someDoors(x){
+ 	
+ 	if (x === 1) {return alert("you win a car");
+ 	}
+ 	
+ 	if (x === 2) {return alert("you win a boat");
+ 	} 
+ 	
+ 	if (x === 3) {return alert("you win a trip");
+  	}	
+};
+
+someDoors(door);
+
+
+
